c16866c11169d5892003f7373951eb962cda9b3e
StevenAJ23 <roseroariel23@gmail.com>
2025-12-19 21:07:49 -0500

    Update Trabajo.php

--- app/Models/Trabajo.php
+++ app/Models/Trabajo.php
@@ -19,31 +19,29 @@ class Trabajo extends Model
         'estado'
     ];
 
-    // Obtener todos los trabajos (solo activos)
     public static function getTrabajos()
     {
         return self::all();
     }
 
-    // Obtener un trabajo por ID
+    
     public static function getTrabajoById($id)
     {
         return self::find($id);
     }
 
-    // Crear un nuevo trabajo
+    
     public static function createTrabajo($data)
     {
         return self::create($data);
     }
 
-    // Actualizar un trabajo existente
     public static function updateTrabajo($id, $data)
     {
         return self::where('id', $id)->update($data);
     }
 
-    // Eliminar un trabajo (SOFT DELETE)
+    
     public static function deleteTrabajo(Trabajo $trabajo)
     {
         $trabajo->delete();
